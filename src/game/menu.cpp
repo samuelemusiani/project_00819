a78d0a8a5423fa4f33c1a162b6b471b921147f5a
Lele <emanuele.2003@yahoo.it>
2023-04-01 10:24:45 +0200

    First implementations of the menu button. UP and DOWN keys work.

--- src/game/menu.cpp
+++ src/game/menu.cpp
@@ -18,13 +18,39 @@ void Menu::drawMenu() {
 	Draw::drawText(3, (COLS - 18)/2, "|");
 	Draw::drawText(3, (COLS + 15)/2, "|");
 
-	// Scrive le opzioni del menu
-	for (int i = 0 ; i < NUMBER_OF_OPTIONS; i++)
-	{
-		Draw::drawText(posY + 2*i , posX - options[i].length()/2, options[i].c_str());
-		
+	while (!flag) {
+		// Scrive le opzioni del menu
+		for (int i = 0 ; i < NUMBER_OF_OPTIONS; i++)
+		{
+			Draw::drawText(posY + 2*i , posX - options[i].length()/2, options[i].c_str());
+		}
+		// Highlight the selected option with a different color
+		attron(A_REVERSE);
+		mvprintw(posY + 2*selectedOption, posX - options[selectedOption].length()/2, options[selectedOption].c_str());
+		attroff(A_REVERSE);
+		// Prende l'input dell'utente e cambia la selezione
+		switch (getch()) {
+			case KEY_UP:
+				if (selectedOption > 0) {
+					selectedOption--;
+				}
+				else if (selectedOption == 0) {
+					selectedOption = NUMBER_OF_OPTIONS - 1;
+				}
+				break;
+			case KEY_DOWN:
+				if (selectedOption < NUMBER_OF_OPTIONS - 1) {
+					selectedOption++; 
+				}
+				else if (selectedOption == NUMBER_OF_OPTIONS - 1) {
+					selectedOption = 0;
+				}
+				break;
+			case 10: 
+				this->flag = true;
+				break;
+		}
 	}
-	
-	
+		
 }
 	
