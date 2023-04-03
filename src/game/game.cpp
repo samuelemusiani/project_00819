b9a9e20b1969b2b55f5af11c3a6636ca5c6e5dd4
Lele <emanuele.2003@yahoo.it>
2023-04-03 14:53:04 +0200

    Check terminal size before starting the game and print a warning if it's too small.

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,11 +1,23 @@
 #include "game.hpp"
 #include "menu.hpp"
+#include <unistd.h>
 
 
 Game::Game()
 {
 	this->init();
+	// Se la grandezza del terminale è minore di 44x150, allora printa errore
+	if (LINES < 44 || COLS < 150)
+	{
+		printw("Your terminal is too small. Please resize it at least to 44x150");
+		while (LINES < 44 || COLS < 150)
+		{
+			refresh();
+			sleep(1); // attendi 1 secondo per non mandare la cpu a 100%
+		}
+	}
 }
+
 
 Game::~Game()
 {
