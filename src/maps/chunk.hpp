0c17b7c213f541fdd9958c3b419ead6d61376d38
Lorenzo <lorenzo.peronese@studio.unibo.it>
2023-04-05 02:02:56 +0200

    Added function get_platforms

--- src/maps/chunk.hpp
+++ src/maps/chunk.hpp
@@ -5,10 +5,11 @@
 class Chunk
 {
     private:
-        Platform platforms[MAX_PLATFORMS];
+        static Platform platforms[MAX_PLATFORMS];
         int usedPlatforms;
     public:
         Chunk();
         void add_platform(int x, int y, int len);
         bool is_there_a_platform(int x, int y);
+        Platform* get_platforms(int n);
 };
