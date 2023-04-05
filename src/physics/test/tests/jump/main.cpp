8967c30d459a4041f60b730ab6f932e3cb3dad78
musianisamuele <samuele.musiani.03@gmail.com>
2023-04-05 12:15:28 +0200

    Basic jump with integers values only

--- src/physics/test/tests/jump/main.cpp
+++ src/physics/test/tests/jump/main.cpp
@@ -1,9 +1,21 @@
 #include <ncurses.h>
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "../../../point.hpp"
 #include "../../../vector.hpp"
+#include "../../../body.hpp"
 
-#define PROTAGONIST "T"
+//const char PROTAGONIST = 'T';
+
+void drawChar(phy::Point p) 
+{
+	int x_max;
+	int y_max;
+	getmaxyx(stdscr, y_max, x_max);
+
+	mvprintw(y_max - p.get_yPosition(), p.get_xPosition(), "T");
+}
 
 void drawProtagonist() 
 {
@@ -14,9 +26,22 @@ void drawProtagonist()
 	int x = 0;
 	int y = 0;
 
+	phy::Body b;
+	b.set_position(phy::Point(0, 0));
+	b.set_velocity(phy::Vector(10, 70));
+	b.set_acceleration(phy::Vector(1, -90));
+
 	while(!EXIT)
 	{
-		INPUT_KEY:
+		b.update(1);
+		//b.set_position(phy::Point(x, y));
+
+		drawChar(b.get_position());
+		refresh();
+		//sleep(0.8);
+		std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+		/*
 		ch = getchar();
 		switch (ch)
 		{
@@ -43,6 +68,10 @@ void drawProtagonist()
 					y--;
 				}
 				break;
+
+			case 'g':
+				b.set_velocity(phy::Vector(1, 0));
+				break;
 				
 			case 'e':
 				if(x < COLS - 1 && y > 0) 
@@ -57,11 +86,9 @@ void drawProtagonist()
 				break;
 
 			default:
-				goto INPUT_KEY;
 				break;
 		}
-		mvprintw(y, x, PROTAGONIST);
-		refresh();
+		*/
 	}
 }
 
@@ -76,7 +103,7 @@ int main()
 	noecho();
 	keypad(stdscr, TRUE);
 
-	//drawProtagonist();
+	drawProtagonist();
 	basicJump();
 
 	endwin();
