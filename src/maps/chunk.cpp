33e8c3888de26cfeb104a2bce38160b219d06cb4
lorenzoperonese <118299372+lorenzoperonese@users.noreply.github.com>
2023-04-07 19:23:04 +0200

    Fixed function get_chunk, added function get_platforms (#29)
    * Fix function get_chunk
    
    * Function get_chunk, but prettier
    
    * Added function get_platforms
    
    * Test
    
    * Test pt.2
    
    * Class platform now uses phy::Point type
    
    * Bug fixed
    
    * Fix class Map and function get_platform again
    
    * Fixed memory leak
    
    * Sistemato commento
    
    * Fixed multiple definitions
    
    * Added vector iostream temporarily

--- src/maps/chunk.cpp
+++ src/maps/chunk.cpp
@@ -1,28 +1,25 @@
 #include "chunk.hpp"
 
-Chunk::Chunk()
+void Chunk::add_platform(phy::Point pos, int len)
 {
-    this->usedPlatforms = 0;
+    Platform p = Platform(pos,len);
+    this->platforms.push_back(p);
 }
 
-void Chunk::add_platform(int x, int y, int len)
-{
-    if(this->usedPlatforms < MAX_PLATFORMS)
-    {
-        this->platforms[this->usedPlatforms].set_xPosition(x);
-        this->platforms[this->usedPlatforms].set_yPosition(y);
-        this->platforms[this->usedPlatforms].set_length(len);
-        this->usedPlatforms++;
-    }
-
-}
-
-bool Chunk::is_there_a_platform(int x, int y){
+bool Chunk::is_there_a_platform(phy::Point pos){
     //true if there is a platform, otherwise false
-    for(int i=0;i<this->usedPlatforms;i++){
-        if(y==this->platforms[i].get_yPosition())
-            if(x >= this->platforms[i].get_xPosition() && x <= (this->platforms[i].get_xPosition())+this->platforms[i].get_length()-1)
+    for(int i=0;i<this->platforms.size();i++){
+        if(pos.get_yPosition()==platforms[i].get_position().get_yPosition())
+            if(pos.get_xPosition() >= this->platforms[i].get_position().get_xPosition() && pos.get_xPosition() <= (this->platforms[i].get_position().get_xPosition()) + this->platforms[i].get_length()-1)
                 return true;
     }
     return false;
+}
+
+/* This method return a pointer to an array of Platforms in which are stored all the platforms that are in the current Chunk.
+ * To get this pointer you first need to instantiate a Map object, then you can use the method get_chunk(...) to get
+ * the desired Chunk,and then you can call the get_platforms() method. */
+std::vector<Platform>* Chunk::get_platforms()
+{
+    return &this->platforms;
 }
--- src/maps/map.cpp
+++ src/maps/map.cpp
@@ -1,28 +1,35 @@
 #include "map.hpp"
+#include "../../etc/logs/logs.hpp"
 // A chunk is 148x42
+
+Map::Map()
+{
+    initialize_chunks();
+}
+
 void Map::initialize_chunks()
 {
     // 1st chunk
-    this->possible_chunks[0].add_platform(46, 3, 11);
-    this->possible_chunks[0].add_platform(112, 3, 15);
-    this->possible_chunks[0].add_platform(21, 8, 23);
-    this->possible_chunks[0].add_platform(86, 8, 21);
-    this->possible_chunks[0].add_platform(130, 8, 9);
-    this->possible_chunks[0].add_platform(2, 12, 11);
-    this->possible_chunks[0].add_platform(110, 14, 24);
-    this->possible_chunks[0].add_platform(23, 16, 10);
-    this->possible_chunks[0].add_platform(39, 19, 10);
-    this->possible_chunks[0].add_platform(85, 19, 26);
-    this->possible_chunks[0].add_platform(56, 23, 13);
-    this->possible_chunks[0].add_platform(80, 27, 19);
-    this->possible_chunks[0].add_platform(20, 29, 33);
-    this->possible_chunks[0].add_platform(95, 32, 22);
-    this->possible_chunks[0].add_platform(0, 36, 28);
-    this->possible_chunks[0].add_platform(97, 37, 31);
+    this->possible_chunks[0].add_platform(phy::Point(46,3), 11);
+    this->possible_chunks[0].add_platform(phy::Point(112,3), 15);
+    this->possible_chunks[0].add_platform(phy::Point(21,8), 23);
+    this->possible_chunks[0].add_platform(phy::Point(86,8), 21);
+    this->possible_chunks[0].add_platform(phy::Point(130,8), 9);
+    this->possible_chunks[0].add_platform(phy::Point(2,12), 11);
+    this->possible_chunks[0].add_platform(phy::Point(110,14), 24);
+    this->possible_chunks[0].add_platform(phy::Point(23,16), 10);
+    this->possible_chunks[0].add_platform(phy::Point(39,19), 10);
+    this->possible_chunks[0].add_platform(phy::Point(85,19), 26);
+    this->possible_chunks[0].add_platform(phy::Point(56,23), 13);
+    this->possible_chunks[0].add_platform(phy::Point(80,27), 19);
+    this->possible_chunks[0].add_platform(phy::Point(20,29), 33);
+    this->possible_chunks[0].add_platform(phy::Point(95,32), 22);
+    this->possible_chunks[0].add_platform(phy::Point(0,36), 28);
+    this->possible_chunks[0].add_platform(phy::Point(97,37), 31);
 }
 
-Chunk Map::get_chunk()
+Chunk Map::get_chunk(int n)
 {
-    //da randomizzare
-    return this->possible_chunks[0];
+    if(n>=0)
+        return this->possible_chunks[0];
 }
