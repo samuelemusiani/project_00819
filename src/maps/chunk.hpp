b9710f617c7d896085ae42223b9f59824b4ad8ae
Samuele Musiani <samuele.musiani.03@gmail.com>
2023-04-09 15:42:09 +0200

    Samu nostd::vector (#36)
    * First implementation of a vector of int
    
    * Split in muliple files
    
    * Refactoring + added resize
    
    * Added assignment of the [] operator
    
    * Tanslate from int to template T
    
    * Delete obsolete files
    
    * Moved nostd/ to etc/
    
    * Fixed includes
    
    * Modified chunk class to use nostd::vector
    
    * Fix bug and improved resize() method

--- src/maps/chunk.hpp
+++ src/maps/chunk.hpp
@@ -1,16 +1,16 @@
 #include "platform.hpp"
-#include <vector>
+#include "../../etc/nostd/vector.hpp"
 #ifndef MAPS_CHUNK
 #define MAPS_CHUNK
 
 class Chunk
 {
     private:
-        std::vector<Platform> platforms;
+        nostd::vector<Platform> platforms;
     public:
         void add_platform(phy::Point pos, int len);
         bool is_there_a_platform(phy::Point pos);
-        std::vector<Platform>* get_platforms();
+        nostd::vector<Platform>* get_platforms();
 };
 
 #endif
