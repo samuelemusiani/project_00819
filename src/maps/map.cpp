2e551bf3d6ba06056595d7107d1deda13bef8fe8
Lorenzo <lorenzo.peronese@studio.unibo.it>
2023-04-14 19:10:02 +0200

    Added new test map

--- src/maps/map.cpp
+++ src/maps/map.cpp
@@ -14,43 +14,35 @@ Map::Map(int seed)
 	initialize_chunks();
 }
 
-void Map::initialize_chunks()
-{
-    // 1st chunk
-    this->possible_chunks[0].add_platform(phy::Point(46,3), 11);
-    this->possible_chunks[0].add_platform(phy::Point(112,3), 15);
-    this->possible_chunks[0].add_platform(phy::Point(21,8), 23);
-    this->possible_chunks[0].add_platform(phy::Point(86,8), 21);
-    this->possible_chunks[0].add_platform(phy::Point(130,8), 9);
-    this->possible_chunks[0].add_platform(phy::Point(2,12), 11);
-    this->possible_chunks[0].add_platform(phy::Point(110,14), 24);
-    this->possible_chunks[0].add_platform(phy::Point(23,16), 10);
-    this->possible_chunks[0].add_platform(phy::Point(39,19), 10);
-    this->possible_chunks[0].add_platform(phy::Point(85,19), 26);
-    this->possible_chunks[0].add_platform(phy::Point(56,23), 13);
-    this->possible_chunks[0].add_platform(phy::Point(80,27), 19);
-    this->possible_chunks[0].add_platform(phy::Point(20,29), 33);
-    this->possible_chunks[0].add_platform(phy::Point(95,32), 22);
-    this->possible_chunks[0].add_platform(phy::Point(0,36), 28);
-    this->possible_chunks[0].add_platform(phy::Point(97,37), 31);
+void Map::initialize_chunks() {
+	// 1st chunk
+	this->possible_chunks[0].add_platform(phy::Point(46, 3), 11);
+	this->possible_chunks[0].add_platform(phy::Point(112, 3), 15);
+	this->possible_chunks[0].add_platform(phy::Point(21, 8), 23);
+	this->possible_chunks[0].add_platform(phy::Point(86, 8), 21);
+	this->possible_chunks[0].add_platform(phy::Point(130, 8), 9);
+	this->possible_chunks[0].add_platform(phy::Point(2, 12), 11);
+	this->possible_chunks[0].add_platform(phy::Point(110, 14), 24);
+	this->possible_chunks[0].add_platform(phy::Point(23, 16), 10);
+	this->possible_chunks[0].add_platform(phy::Point(39, 19), 10);
+	this->possible_chunks[0].add_platform(phy::Point(85, 19), 26);
+	this->possible_chunks[0].add_platform(phy::Point(56, 23), 13);
+	this->possible_chunks[0].add_platform(phy::Point(80, 27), 19);
+	this->possible_chunks[0].add_platform(phy::Point(20, 29), 33);
+	this->possible_chunks[0].add_platform(phy::Point(95, 32), 22);
+	this->possible_chunks[0].add_platform(phy::Point(0, 36), 28);
+	this->possible_chunks[0].add_platform(phy::Point(97, 37), 31);
 
 	// 2nd chunk
-	this->possible_chunks[1].add_platform(phy::Point(46,3), 11);
-	this->possible_chunks[1].add_platform(phy::Point(112,3), 15);
-	this->possible_chunks[1].add_platform(phy::Point(21,8), 23);
-	this->possible_chunks[1].add_platform(phy::Point(86,8), 21);
-	this->possible_chunks[1].add_platform(phy::Point(131,8), 9);
-	this->possible_chunks[1].add_platform(phy::Point(2,12), 11);
-	this->possible_chunks[1].add_platform(phy::Point(111,14), 24);
-	this->possible_chunks[1].add_platform(phy::Point(23,16), 11);
-	this->possible_chunks[1].add_platform(phy::Point(39,19), 11);
-	this->possible_chunks[1].add_platform(phy::Point(85,19), 26);
-	this->possible_chunks[1].add_platform(phy::Point(56,23), 13);
-	this->possible_chunks[1].add_platform(phy::Point(81,27), 19);
-	this->possible_chunks[1].add_platform(phy::Point(21,29), 33);
-	this->possible_chunks[1].add_platform(phy::Point(95,32), 22);
-	this->possible_chunks[1].add_platform(phy::Point(1,36), 28);
-	this->possible_chunks[1].add_platform(phy::Point(97,37), 31);
+	this->possible_chunks[1].add_platform(phy::Point(60, 2), 12);
+	this->possible_chunks[1].add_platform(phy::Point(24, 6), 28);
+	this->possible_chunks[1].add_platform(phy::Point(80, 8), 22);
+	this->possible_chunks[1].add_platform(phy::Point(0, 15), 11);
+	this->possible_chunks[1].add_platform(phy::Point(114, 17), 6);
+	this->possible_chunks[1].add_platform(phy::Point(29, 21), 24);
+	this->possible_chunks[1].add_platform(phy::Point(77, 24), 33);
+	this->possible_chunks[1].add_platform(phy::Point(38, 30), 23);
+	this->possible_chunks[1].add_platform(phy::Point(77, 36), 15);
 }
 
 Chunk Map::get_chunk(int n)
