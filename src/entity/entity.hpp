c620374a0e78a7fdc5de809b969d994625701461
Omar Ayache <ayache.omar@gmail.com>
2023-04-15 04:18:00 +0200

    added bool field in entity class

--- src/entity/entity.hpp
+++ src/entity/entity.hpp
@@ -1,14 +1,11 @@
 #include "../../ect/nostd/string.hpp"
 #include "../physics/point.hpp"
 
-
-#define N 10
-
 class Entity {
 protected:
   nostd::string id;
   phy::Point p;
-
+  bool alive;
 public:
 
   Entity(nostd::string id, phy::Point p);
