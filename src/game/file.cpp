cdf0b805370d2caa81a4bd0d8e7ee2d9d3221142
Lorenzo Peronese <lorenzo.peronese@studio.unibo.it>
2023-04-20 22:42:20 +0200

    Class File-reading complete

--- src/game/file.cpp
+++ src/game/file.cpp
@@ -24,18 +24,19 @@ void File::initSettings() {
 		if(openFile(&file,"./settings.txt","w"))
 		{
 			file << "{ JUMP KING }\n\n[ KeyBindings ]\nml=s\nmr=d\njl=a\njr=f\njp=v\nsh=o\nbb=p\not=esc\n\n"
-					"[ Calibration ]\ncalibr=0\n\n[ Audio ]\nvol=5\n\n[ Sensitivity ]\nsens=5\n\n";
+					"[ Calibration ]\ncalibr=-1\n\n[ Audio ]\nvol=10\n\n[ Sensitivity ]\nsens=10\n\n";
 			file.close();
+			getSettings();
 		}
 }
 
-void File::saveSettings(Settings s)
+void File::saveSettings()
 {
 	std::fstream file;
 	if(openFile(&file,"./settings.txt","w"))
-		file << /*"{ JUMP KING }\n\n[ KeyBindings ]\nml="<<s.getKeys[0]<<"\nmr="<<s.getKeys[1]<<"\njl="<<s.getKeys[2]<<"\n"
-				"jr="<<s.getKeys[3]<<"\njp="<<s.getKeys[4]<<"\nsh="<<s.getKeys[5]<<"\nbb="<<s.getKeys[6]<<"\not="<<s.getKeys[7]<<"\n\n"
-				"[ Calibration ]\ncalibr="<<s.getCalibration<<"\n\n[ Audio ]\nvol="<<s.getVolume<<"\n\n[ Sensitivity ]\n"<<"sens="<<s.getVolume<<*/"\n";
+		file << "{ JUMP KING }\n\n[ KeyBindings ]\nml="<<SETTINGS_CONTROL_KEYS[0]<<"\nmr="<<SETTINGS_CONTROL_KEYS[1]<<"\njl="<<SETTINGS_CONTROL_KEYS[2]<<"\n"
+				"jr="<<SETTINGS_CONTROL_KEYS[3]<<"\njp="<<SETTINGS_CONTROL_KEYS[4]<<"\nsh="<<SETTINGS_CONTROL_KEYS[5]<<"\nbb="<<SETTINGS_CONTROL_KEYS[6]<<"\not="<<SETTINGS_CONTROL_KEYS[7]<<"\n\n"
+				"[ Calibration ]\ncalibr="<<SETTINGS_CALIBRATION<<"\n\n[ Audio ]\nvol="<<SETTINGS_VOLUME_LEVEL<<"\n\n[ Sensitivity ]\n"<<"sens="<<SETTINGS_SENSITIVITY_LEVEL<<"\n";
 	file.close();
 }
 
@@ -80,7 +81,7 @@ void File::appendSave(Map m,std::string name)
 	std::fstream file;
 	if(openFile(&file,"./save.txt","app")) {
 		file << "[ Name: " << name << " ]\nSeed: " << m.getSeed().getSeed()
-				 << "\nCoins&Enemies: " << m.getCoinsAndEnemies() << "\n" << dateAndTime() << "\n\n";
+				 << "\nCoins&Enemies: " << m.getCoinsAndEnemies() << "\nLastSave: " << dateAndTime() << "\n\n";
 		file.close();
 	}
 }
@@ -95,7 +96,7 @@ void File::updateSave(Map m)
 	std::string search = "Seed: "+std::to_string(m.getSeed().getSeed());
 	if(openFile(&tmp,"./tmp.txt","w") && openFile(&file,"./save.txt","r"))
 	{
-		while(getline(file,buff) && !found)
+		while(!found && getline(file,buff))
 		{
 			tmp << buff << "\n";
 			if(buff==search)
@@ -104,7 +105,7 @@ void File::updateSave(Map m)
 		getline(file,buff); // this is the 1st line that has to be replaced
 		tmp << "Coins&Enemies: " << m.getCoinsAndEnemies() << "\n";
 		getline(file,buff); // this is the 2nd line that has to be replaced
-		tmp << dateAndTime() << "\n\n";
+		tmp << "LastSave: " << dateAndTime() << "\n\n";
 		while(getline(file,buff))
 			tmp << buff << "\n";
 		file.close();
@@ -116,7 +117,7 @@ void File::updateSave(Map m)
 void File::saveMap(Map m,std::string name)
 {
 	if(!isAlreadySaved(m)) {
-		if (name != "")
+		if (!name.empty())
 			appendSave(m, name);
 		else
 			appendSave(m, "Player"); // this shouldn't happen
@@ -135,12 +136,13 @@ void File::changeName(std::string oldName,std::string newName)
 	std::string search = "[ Name: " + oldName + " ]";
 	if(openFile(&tmp,"./tmp.txt","w") && openFile(&file,"./save.txt","r"))
 	{
-		while(getline(file,buff) && !found)
+		while(!found && getline(file,buff))
 		{
 			count++;
 			if(buff==search)
 				found=true;
 		}
+		if(!found)	return;
 		file.clear();
 		file.seekg(0);
 		for(int i=0;i<count;i++)
@@ -158,7 +160,7 @@ void File::changeName(std::string oldName,std::string newName)
 	}
 }
 
-nostd::vector<std::string>* File::getNames()
+nostd::vector<std::string> File::getNames()
 {
 	static nostd::vector<std::string> names;
 	std::fstream file;
@@ -166,32 +168,32 @@ nostd::vector<std::string>* File::getNames()
 	if(openFile(&file,"./save.txt","r"))
 	{
 		while(getline(file,buff))
-			if(buff[0] == '[')
+			if(buff.substr(0,7)=="[ Name:")
 				names.push_back(buff.substr(8,buff.length()-10));
 	}
 	file.close();
-	return &names;
+	return names;
 }
 
-nostd::vector<std::string>* File::getLastModify()
+nostd::vector<std::string> File::getLastSave()
 {
-	static nostd::vector<std::string> lastModify;
+	static nostd::vector<std::string> lastSave;
 	std::fstream file;
 	std::string buff;
 	if(openFile(&file,"./save.txt","r"))
 	{
 		while(getline(file,buff))
-			if(buff.substr(0,2)=="20")
-				lastModify.push_back(buff.substr(0,buff.length()-1));
+			if(buff.substr(0,9)=="LastSave:")
+				lastSave.push_back(buff.substr(10,buff.length()-10));
 	}
 	file.close();
-	return &lastModify;
+	return lastSave;
 }
 
 bool File::nameAlreadyInUse(std::string name)
 {
 	std::fstream file;
-	std::string search = "[ " + name + " ]";
+	std::string search = "[ Name: " + name + " ]";
 	std::string buff;
 	if(openFile(&file,"./save.txt","r"))
 	{
@@ -206,4 +208,61 @@ bool File::nameAlreadyInUse(std::string name)
 	}
 	else
 		return false; // file does not exist
+}
+
+Map File::getMap(std::string name)
+{
+	std::fstream file;
+	std::string search = "[ Name: " + name + " ]";
+	std::string buff;
+	std::string anotherBuff;
+	bool found = false;
+	if(openFile(&file,"./save.txt","r"))
+	{
+		while (!found && getline(file, buff))
+			if (buff == search)
+				found = true;
+		if(!found)
+		{
+			Map casualMap;
+			return casualMap; // this shouldn't happen
+		}
+		getline(file, buff);
+		getline(file, anotherBuff);
+		Map m(std::stoi(buff.substr(6, buff.length() - 6)), anotherBuff.substr(15, anotherBuff.length() - 15));
+		return m;
+
+	}
+	Map casualMap2;
+	return casualMap2; // this shouldn't happen
+}
+
+void File::getSettings()
+{
+	std::fstream file;
+	std::string buff;
+	bool found = false;
+	if(openFile(&file,"./setting.txt"))
+	{
+		while(!found && getline(file,buff))
+			if(buff == "[ KeyBindings ]")
+				found = true;
+		for(int i=0;i<8;i++)
+		{
+			getline(file,buff);
+			SETTINGS_CONTROL_KEYS[i] = buff.substr(3,buff.length()-3);
+		}
+		getline(file,buff); // empty line
+		getline(file,buff); // [ Calibration ]
+		getline(file,buff);
+		SETTINGS_CALIBRATION = stoi(buff.substr(7,buff.length()-7));
+		getline(file,buff); // empty line
+		getline(file,buff); // [ Audio ]
+		getline(file,buff);
+		SETTINGS_VOLUME_LEVEL = stoi(buff.substr(4,buff.length()-4));
+		getline(file,buff); // empty line
+		getline(file,buff); // [ Sensitivity ]
+		getline(file,buff);
+		SETTINGS_SENSITIVITY_LEVEL = stoi(buff.substr(5,buff.length()-5));
+	}
 }
--- src/game/global.cpp
+++ src/game/global.cpp
@@ -1,4 +1,7 @@
 #include "global.hpp"
 
-int SETTINGS_VOLUME_LEVEL = 3;
-int SETTINGS_SENSITIVITY_LEVEL = 15 ;
+
+std::string SETTINGS_CONTROL_KEYS[8] = { "q","w","e","r","t","y","z","x" };
+int SETTINGS_CALIBRATION = 155;
+int SETTINGS_VOLUME_LEVEL = 15;
+int SETTINGS_SENSITIVITY_LEVEL = 5;
