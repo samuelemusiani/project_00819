573b8b29fa9e01b583b4adaa6e4850f3026c4e54
Musiani Samuele <samuele.musiani.03@gmail.com>
2023-05-08 10:31:11 +0200

    Fixed bug when exit game: illegal instrution

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -82,6 +82,7 @@ void Game::run()
 		case 27:
 			{
 				if (exitGame() == true) exit = true;
+				break;
 			}
 		}
 		
@@ -129,6 +130,7 @@ bool Game::exitGame(){
 		
 	}
 	if (selected == 0) return true;
+	else return false;
 }
 
 void Game::start()
