98c7d514ed3696e81d0301e57c98c05162a20858
Musiani Samuele <samuele.musiani.03@gmail.com>
2023-06-28 22:58:59 +0200

    More enemies and fewer coins when advancing

--- src/engine/random.cpp
+++ src/engine/random.cpp
@@ -26,12 +26,12 @@ void Random::seedSrand(Seed seed, int chunk, int offset)
 
 int Random::generateEnemies(Seed seed, int chunk) {
 	seedSrand(seed, chunk, 11);
-	return (2 + rand() % 3); // enemies range: 2-4
+    return std::min(7, chunk / 2 + rand() % 3);
 }
 
 int Random::generateCoins(Seed seed, int chunk) {
 	seedSrand(seed, chunk, 13);
-	return (3 + rand() % 4); // coins range: 3-6
+	return std::max(1, 3 + rand() % 4 - chunk / 3);
 }
 
 int Random::generateChunk(Seed seed, int chunk) {
