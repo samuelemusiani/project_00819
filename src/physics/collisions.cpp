94af266f20955c2747c0a8ec520782f7f03153b0
Musiani Samuele <samuele.musiani.03@gmail.com>
2023-08-08 16:19:45 +0200

    Removed comments on collisions

--- src/physics/collisions.cpp
+++ src/physics/collisions.cpp
@@ -5,7 +5,7 @@
  *
  * Author: Musiani Samuele #0001069143
  *
- * collision.cpp: Implentation of the collision function.
+ * collision.cpp: Implentation of the collision functions.
  */
 
 #include <math.h>
@@ -27,25 +27,6 @@ static bool hasJumped(phy::Body &body);
 static bool is_on_a_platform(phy::Body &body, Chunk *chunk);
 static void borderCollision(phy::Body &body);
 
-/*
-   Le possibilità per implementare la funzione sono molte.
-   L'idea è che prende un putatore a body e che lo aggiorna dopo 'time' tempo
-   tenendo conto delle possibili collisioni che avvengono sul chunk.
-
-   1) Posso vedere la velocità di body, la su accelerazione e il tempo in modo
-   che abbia due possibili sottoscelte: se il corpo dopo lo spotamente dato da
-   'time' si è spostato di un solo blocco mi basta controllare che quel blocco
-   non sia una piattaforma e ho finito. Se invece si sposta di più di un blocco
-   faccio aggiornamenti più piccoli suddividendo il tempo in maniera tale che
-   si sposti sempre di un blocco e faccio sempre il check se ha toccato una
-   piattaforma.
-
-   2) Posso calcolare tutta la traiettoria del punto e poi a posteriore fare un
-   check se ho fatto una collisione. Solo che non saprei quando finire di
-   calcolare la traiettoria (visto che potrebbe anche andare in caduta libera)
-   e sopratutto avrei fatto calcoli inutili se la collissione fosse all'inzio.
-*/
-
 void phy::updateWithCollisions(phy::Body &body, double time, Chunk chunk) {
 
   // Check collision with the map boders
@@ -57,7 +38,6 @@ void phy::updateWithCollisions(phy::Body &body, double time, Chunk chunk) {
     } else {
       // FREE FALL
 
-      // Devo continuare a settare l'accelerazione?
       body.set_acceleration(phy::Vector(phy::GRAVITY_ACCELERATION, -90));
 
       phy::Body old_body = body;
@@ -92,7 +72,6 @@ void phy::updateWithCollisions(phy::Body &body, double time, Chunk chunk) {
     phy::Body old_body = body;
     body.update(time);
 
-    // Faccio tutto con int o metto i precisePoint?
     int old_xPos = round(old_body.get_precisePosition().get_xPosition());
     int old_yPos = round(old_body.get_precisePosition().get_yPosition());
     int new_xPos = round(body.get_precisePosition().get_xPosition());
@@ -138,12 +117,11 @@ void phy::updateWithCollisions(phy::Body &body, double time, Chunk chunk) {
 
     // 4 -> bad...really bad
     case 4:
-      // I don't know what to do :)
       body.set_position(old_body.get_position());
       break;
 
+    // Something went wrong...
     default:
-      // Something went wrong...
       resetVelocityAcceleration(body);
       break;
     }
@@ -160,13 +138,15 @@ void phy::updateWithCollisions(Bullet &bullet, double time, Chunk chunk) {
   bullet.set_velocity(tmp.get_velocity());
 }
 
-// 0 -> no collision
-// 1 -> basic collision in the x direction (the body moved only on one block)
-// 2 -> basic collision in the y direction (the body moved only on one block)
-// 3 -> complex collision (the body moved one block in the x and one block in
-// the y direction) 4 -> bad...really bad
-
-/* This function detect the type of collision that append base on the old and
+/*
+ * 0 -> no collision
+ * 1 -> basic collision in the x direction (the body moved only on one block)
+ * 2 -> basic collision in the y direction (the body moved only on one block)
+ * 3 -> complex collision (the body moved one block in the x and one block in
+ * the y direction)
+ * 4 -> bad...really bad
+ *
+ * This function detect the type of collision that append base on the old and
  * the new position of the player.
  */
 static int detectCollision(int old_xPos, int old_yPos, int new_xPos,
