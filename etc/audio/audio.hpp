a267b46cc389cd3d855127fbc2852ea69f6d89b1
Musiani Samuele <samuele.musiani.03@gmail.com>
2023-08-10 14:30:06 +0200

    Added submodule but CMake does not work

---
+++ etc/audio/audio.hpp
@@ -0,0 +1,31 @@
+#ifndef ETC_AUDIO_AUDIO
+#define ETC_AUDIO_AUDIO
+
+#include "../nostd/string.hpp"
+
+#include <thread>
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio/miniaudio.h"
+
+class Audio {
+private:
+  ma_engine _engine;
+  bool _is_engine_inizilized;
+
+  std::thread _music_thread;
+  bool _need_to_stop_music = false;
+
+  void _play_music(nostd::string s);
+
+public:
+  Audio();
+  ~Audio();
+
+  void play_music(nostd::string path);
+  void stop_music();
+  // bool play_fx(nostd::string s);
+  // bool stop();
+};
+
+#endif
